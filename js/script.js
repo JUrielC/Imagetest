80a069ef3d0e7fd800ffb87cf63a1a75caf57548
JUrielC <uri.jucv@gmail.com>
2024-11-15 10:11:30 -0600

    .

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 console.log("hola")
 
-fetch('https://www.misabogados.com.co/hubfs/Imported_Blog_Media/bienes-de-uso-publico.jpg',
+fetch('https://s3-giant-motors-crm-media-dev.s3.amazonaws.com/Products/phygital-logos-new/j7_25.png',
+    {
+
+        method: 'GET',
+        cache: 'no-cache' 
+
+    }
 )
     .then(response => {
         if (response.ok) {
@@ -24,7 +30,13 @@ fetch('https://www.misabogados.com.co/hubfs/Imported_Blog_Media/bienes-de-uso-pu
     });
 
 
-fetch('https://s3-giant-motors-crm-media-dev.s3.us-east-1.amazonaws.com/Products/phygital-logos-new/36_30_E_J7_vE+J7+2024_Logo.jpg')
+fetch('https://s3-giant-motors-crm-media-dev.s3.us-east-1.amazonaws.com/Products/phygital-logos-new/36_30_E_J7_vE+J7+2024_Logo.jpg',
+    {
+
+        method: 'GET',
+        cache: 'no-cache'
+
+    })
     .then(response => {
         if (response.ok) {
             return response.blob();
@@ -38,6 +50,33 @@ fetch('https://s3-giant-motors-crm-media-dev.s3.us-east-1.amazonaws.com/Products
         imgElement.alt = 'Descripción de la imagen';
         imgElement.width = 500;
         const container = document.getElementById('image-container-2');
+        imgElement.src = imgUrl;
+        container.appendChild(imgElement);
+    })
+    .catch(error => {
+        console.error('Error al cargar la imagen:', error);
+    });
+
+fetch('https://s3-giant-motors-crm-media-dev.s3.amazonaws.com/ecommerce/1731526295274719_images_d32f0fbb35c4c87c81120128141e24a5.jpg',
+   /*  {
+
+        method: 'GET',
+        cache: 'no-cache'
+
+    } */)
+    .then(response => {
+        if (response.ok) {
+            return response.blob();
+        }
+        throw new Error('No se pudo cargar la imagen');
+    })
+    .then(blob => {
+        const imgUrl = URL.createObjectURL(blob);
+
+        const imgElement = document.createElement('img');
+        imgElement.alt = 'Descripción de la imagen';
+        imgElement.width = 500;
+        const container = document.getElementById('image-container-3');
         imgElement.src = imgUrl;
         container.appendChild(imgElement);
     })
